0da85f7acef75e0190c04574ed37825089f2e450
kalpshah485 <kalpshah485@gmail.com>
2022-02-21 18:01:38 +0530

    feat:Added todo button functionalities: hover to show button if item is checked then compulsory show btn and text color changes.

--- src/TodoList/Todos/TodoItem/TodoItem.js
+++ src/TodoList/Todos/TodoItem/TodoItem.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TodoItem(props) {
+  const [display, setDisplay] = useState(props.todo.checked);
+  const handleDisplay = () => {
+    if (props.todo.checked) {
+      setDisplay(true);
+    }else{
+      if (display) {
+        setDisplay(false);
+      }else{
+        setDisplay(true);
+      }
+    }
+  }
   return (
-      <div className="row todo-item">
-        <span className={`col-9 ${props.completed ? 'text-muted' : ''}`}>{props.todo.title}</span>
-        <span className={`col-2 offset-1 ${props.display ? props.display: 'd-none'} icon`}>
+      <div className="row todo-item" onMouseEnter={handleDisplay} onMouseLeave={handleDisplay}>
+        <span className={`col-9 todo-title ${props.todo.checked ? 'text-muted' : ''}`}>{props.todo.title}</span>
+        <span className={`col-2 offset-1 ${display ? 'd-inline': 'd-none'} icon`} onClick={() => props.toggleCheck(props.index)}>
           <i className="fa-solid fa-right-left"></i>
         </span>
       </div>
