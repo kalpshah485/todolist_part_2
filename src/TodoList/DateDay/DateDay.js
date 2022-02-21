dd7cc7fb673bc2b263aa1d92c58901ce50451313
kalpshah485 <kalpshah485@gmail.com>
2022-02-21 16:07:04 +0530

    feat:Added dynamic date.

--- src/TodoList/DateDay/DateDay.js
+++ src/TodoList/DateDay/DateDay.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 class DateDay extends React.Component {
     render() {
+        const today = new Date();
+        const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         return (
             <div className="row m-0 dateday">
-                <div className="col-1 p-0 date">12</div>
-                <div className="col p-0 month-year"><span className="month">JAN</span><br /><span className="year">2019</span></div>
-                <div className="col p-0 offset-3 day">TUESDAY</div>
+                <div className="col-1 p-0 date">{today.getDate()}</div>
+                <div className="col p-0 month-year"><span className="month">{months[today.getMonth()]}</span><br /><span className="year">{today.getFullYear()}</span></div>
+                <div className="col p-0 offset-3 day">{days[today.getDay()].toUpperCase()}</div>
             </div>
         )
     }
